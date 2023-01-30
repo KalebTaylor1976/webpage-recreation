029f9e8e22a2427f4212513915544ffc641abe85
Kaleb Taylor <kaleb.taylor1976@gmail.com>
2023-01-29 16:20:24 -0800

    add html and head element

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 // User Interface Logic
 window.onload = function(){
- const h1 = document.querySelector("h1");
- h1.remove();
- const h1new = document.createElement(h1new);
- h1new.prepend("Webpage Recreation Practice")
-};
+  const h1 = document.querySelector("h1");
+  h1.remove();
+ 
+ };
+ // create html element
+ const html = document.createElement("html");
+ html.setAttribute("lang", "en-US");
+ document.appendChild(html);
+ 
+ // create head element
+ const head = document.createElement("head");
+ html.appendChild(head);
