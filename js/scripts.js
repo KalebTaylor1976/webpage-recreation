fd1a83ee9fa476b55948e21b58b2d644cc81cf44
Kaleb Taylor <kaleb.taylor1976@gmail.com>
2023-01-29 16:06:55 -0800

    add correct syntax for .remove on h1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 // User Interface Logic
 window.onload = function(){
+ const h1 = document.querySelector("h1");
+ h1.remove();
+ const h1new = document.createElement(h1new);
+ h1new.prepend("Webpage Recreation Practice")
 };
-document.querySelector("h1").innertext = "Webpage Recreation Practice";
